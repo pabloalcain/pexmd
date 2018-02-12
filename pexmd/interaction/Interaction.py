07a54e0a81f1b374f9aed422fd2f8f0896986740
Pablo Alcain <pabloalcain@gmail.com>
2018-02-12 03:40:16 -0300

    Add interaction and implement LJ (very)brute-force

---
+++ pexmd/interaction/Interaction.py
@@ -0,0 +1,94 @@
+"""
+Main Interaction module.
+"""
+
+import numpy as np
+
+class Interaction(object):
+  """
+  Base Interaction class.
+  """
+  def __init__(self, types):
+    self.types = types
+
+  def forces(self, x, v, t):
+    """
+    Main loop calculation.
+
+    NOTE: This is highly experimental and slow.
+    It is just meant to be a proof of concept for the main loop, but
+    has to change *dramatically*, even in arity, when we want to add
+    lists of neighbors, parallelization and so on.
+    """
+    return np.zeros_like(x)
+
+class ShortRange(Interaction):
+  """
+  Base short-range class
+  """
+  def __init__(self, types, rcut, shift_style='None'):
+    """
+    Base short-range class
+
+    Parameters
+    ----------
+
+    rcut : float
+        The cut radius parameter
+
+    shift_style: {'None', 'Displace', 'Splines'}
+        Shift style when approaching rcut
+    """
+    self.rcut = rcut
+    self.shift_style = shift_style
+    super().__init__(types)
+
+
+class LennardJones(ShortRange):
+  """
+  Lennard-Jones potential
+  """
+  def __init__(self, types, rcut, eps, sigma, shift_style='None'):
+    self.eps = eps
+    self.sigma = sigma
+    super().__init__(types, rcut, shift_style)
+
+  def forces(self, x, v, t):
+    """
+    Calculate Lennard-Jones force
+    """
+    x1 = x[t == self.types[0]]
+    x2 = x[t == self.types[1]]
+    i1 = np.arange(len(x))[t == self.types[0]]
+    i2 = np.arange(len(x))[t == self.types[1]]
+    forces = np.zeros_like(x)
+    # I have to split it to avoid double-counting. Don't want to get
+    # too fancy since it will change when creating neighbor lists
+    if self.types[0] == self.types[1]:
+      for i, s1 in enumerate(x1):
+        for j, s2 in enumerate(x2[i+1:]):
+          f = self._lj(s1, s2)
+          ii = i1[i]
+          jj = i2[j+i+1]
+          forces[ii] += f
+          forces[jj] -= f
+    else:
+      for i, s1 in enumerate(x1):
+        for j, s2 in enumerate(x2):
+          f = self._lj(s1, s2)
+          ii = i1[i]
+          jj = i2[j]
+          forces[ii] += f
+          forces[jj] -= f
+    return forces
+
+
+  def _lj(self, s1, s2):
+    d = np.linalg.norm(s1-s2)
+    if d > self.rcut:
+      return np.zeros_like(s1)
+    ljf = 24*self.eps*(2*self.sigma**12/d**13 - self.sigma**6/d**7)*(s1-s2)
+    if self.shift_style == 'None':
+      return ljf
+    elif self.shift_style == 'Displace':
+      return ljf
---
+++ tests/test_interaction.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `Interaction` module."""
+
+
+import unittest
+from nose.tools import assert_equals, assert_raises
+
+from pexmd import interaction
+import numpy as np
+
+class TestInteraction(unittest.TestCase):
+  """Tests for `Interaction` module."""
+
+  def setUp(self):
+    """Set up test fixtures, if any."""
+    self.four_by3 = np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0],
+                              [-1.0, 0.0, 0.0], [0.0, 1.0, 8.0]])
+    self.three_by3 = np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0],
+                               [-1.0, 0.0, 0.0]])
+    self.four_type = np.array([1, 1, 1, 1], dtype=np.int32)
+    self.two_two_type = np.array([1, 1, 2, 2], dtype=np.int32)
+
+  def tearDown(self):
+    """Tear down test fixtures, if any."""
+    pass
+
+  def test_create_interaction(self):
+    i = interaction.Interaction([1, 1])
+    f = i.forces(self.four_by3, self.four_by3, self.four_type)
+    np.testing.assert_array_almost_equal(f, np.zeros_like(self.four_by3))
+
+
+  def test_create_shortrange(self):
+    interaction.ShortRange([1, 1], 5.4, "None")
+
+  def test_create_lennardjones(self):
+    interaction.LennardJones([1, 1], 5.4, 1.0, 1.0, "None")
+
+  def test_lj_two_noshift(self):
+    lj = interaction.LennardJones([1, 1], 5.4, 1.0, 1.0, "None")
+    f = lj._lj(np.array([2.0**(1.0/6), 0.0, 0.0]),
+               np.array([0.0, 0.0, 0.0]))
+    np.testing.assert_array_almost_equal(f, np.zeros(3))
+    f = lj._lj(np.array([0.0, 1.0, 0.0]),
+               np.array([0.0, 0.0, 0.0]))
+    np.testing.assert_array_almost_equal(f, np.array([0, 24, 0]))
+    f = lj._lj(np.array([0.0, 7.0, 0.0]),
+               np.array([0.0, 0.0, 0.0]))
+    np.testing.assert_array_almost_equal(f, np.zeros(3))
+
+  def test_lj_two_displace(self):
+    lj = interaction.LennardJones([1, 1], 5.4, 1.0, 1.0, "Displace")
+    f = lj._lj(np.array([2.0**(1.0/6), 0.0, 0.0]),
+               np.array([0.0, 0.0, 0.0]))
+    np.testing.assert_array_almost_equal(f, np.zeros(3))
+    f = lj._lj(np.array([0.0, 1.0, 0.0]),
+               np.array([0.0, 0.0, 0.0]))
+    np.testing.assert_array_almost_equal(f, np.array([0, 24, 0]))
+    f = lj._lj(np.array([0.0, 7.0, 0.0]),
+               np.array([0.0, 0.0, 0.0]))
+    np.testing.assert_array_almost_equal(f, np.zeros(3))
+
+  def test_lj_forces_equal(self):
+    lj = interaction.LennardJones([1, 1], 5.4, 1.0, 1.0, "None")
+    f = lj.forces(self.four_by3, self.four_by3, self.four_type)
+    force_by_hand = np.array([[0.0, 0.0, 0.0], [23.63671875, 0.0, 0.0],
+                               [-23.63671875, 0.0, 0.0], [0.0, 0.0, 0.0]])
+    np.testing.assert_array_almost_equal(f, force_by_hand)
+
+  def test_lj_forces_diff(self):
+    lj = interaction.LennardJones([1, 2], 5.4, 1.0, 1.0, "None")
+    f = lj.forces(self.four_by3, self.four_by3, self.two_two_type)
+    force_by_hand = np.array([[24.0, 0.0, 0.0], [-0.36328125, 0.0, 0.0],
+                              [-23.63671875, 0.0, 0.0], [0.0, 0.0, 0.0]])
+    np.testing.assert_array_almost_equal(f, force_by_hand)
